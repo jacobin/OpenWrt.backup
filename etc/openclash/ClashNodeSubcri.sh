26d8e4897ea86d12665f7e2085a123ebca8a7bd6
Jacobin <harly.he@gmail.com>
2025-10-19 14:39:29 +0800

    bTooOldFile

--- etc/openclash/ClashNodeSubcri.sh
+++ etc/openclash/ClashNodeSubcri.sh
@@ -194,8 +194,8 @@ for (( i=1; i<=5; i++ )); do
             # https://stackoverflow.com/questions/16391208/print-a-files-last-modified-date-in-bash
             oldFiletime=$(date -r "${DATA_DIR}/original/${fname}" +%s%3N)
             nowDatetime=$(date +%s%3N)
-            [[ $((nowDatetime - oldFiletime)) -gt $((7*24*60*60)) ]] && gt10days=true || gt10days=false
-            if [[ "${gt10days}" == "true" ]]; then
+            [[ $((nowDatetime - oldFiletime)) -gt $((7*24*60*60)) ]] && bTooOldFile=true || bTooOldFile=false
+            if [[ "${bTooOldFile}" == "true" ]]; then
                 tee_echo "\tFile \"${url}\" is too old and has NOT been updated for more than 7 days"
                 rm -f "${DATA_DIR}/original/${fname}.tmp" > /dev/null 2>&1
                 continue
