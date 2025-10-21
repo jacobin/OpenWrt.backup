239e94fa6863586fdfd4e1b77085655182616c26
Jacobin <harly.he@gmail.com>
2025-10-21 10:07:57 +0800

    wget -p -O/dev/null "http://www.baidu.com" --dns-timeout=10 --connect-timeout=10 --read-timeout=10 --tries=1

--- etc/openclash/ClashNodeSubcri.sh
+++ etc/openclash/ClashNodeSubcri.sh
@@ -148,14 +148,27 @@ unset clashConfigNames
 
 
 ###############################################################################
-## 如果网络还在工作，那么就取消此次的订阅 #####################################
+## 如果连内网都已宕机，那么就取消此次的订阅 ###################################
+###############################################################################
+readarray -t arrSpeedTestResult < <( wget -p -O/dev/null "http://www.baidu.com" --dns-timeout=10 --connect-timeout=10 --read-timeout=10 --tries=1 2>&1 | grep -o "[0-9.]\\+ [KM]*B/s" )
+speedSize=${#arrSpeedTestResult[@]}
+if (( speedSize <= 0 )); then
+    tee_echo "\tThe Baidu is NOT available. Exit this time."
+    singleton_clean_up 1
+fi
+unset arrSpeedTestResult; unset speedSize
+
+
+###############################################################################
+## 如果外网尚且可用，那么就取消此次的订阅 #####################################
 ###############################################################################
 readarray -t arrSpeedTestResult < <( wget -p -O/dev/null "http://www.youtube.com" --dns-timeout=10 --connect-timeout=10 --read-timeout=10 --tries=1 2>&1 | grep -o "[0-9.]\\+ [KM]*B/s" )
 speedSize=${#arrSpeedTestResult[@]}
 if [[ 0 < ${speedSize} ]]; then
-    tee_echo "\tThe network is still available. Exit this time."
+    tee_echo "\tThe Youtube is still available. Exit this time."
     singleton_clean_up 1
 fi
+unset arrSpeedTestResult; unset speedSize
 
 
 ###############################################################################
