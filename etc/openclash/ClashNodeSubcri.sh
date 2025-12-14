2a72093d88a5f07ef77e1a05749f794262537a01
Jacobin <harly.he@gmail.com>
2025-12-14 19:40:21 +0800

    留五个最新的文件

--- etc/openclash/ClashNodeSubcri.sh
+++ etc/openclash/ClashNodeSubcri.sh
@@ -355,10 +355,10 @@ rm_command_string="rm -f"
 useWildcardsForPathsContainingSpaces="/etc/config/openclash.2"
 # https://stackoverflow.com/questions/6897190/problem-listing-files-in-bash-with-spaces-in-directory-path?rq=3
 useWildcardsForPathsContainingSpacesEscaped=`echo "$useWildcardsForPathsContainingSpaces" | sed 's/[[:space:]]/\[[:space:]]/g'`
-readarray -t arrOpenclashConfigBakup < <(ls -tc -1 ${useWildcardsForPathsContainingSpacesEscaped}*)
+readarray -t arrOpenclashConfigBakup < <(ls -v -1 ${useWildcardsForPathsContainingSpacesEscaped}*)
 n=${#useWildcardsForPathsContainingSpaces}  # Number of characters to compare
 bakSize=${#arrOpenclashConfigBakup[@]}
-for (( j=5; j<${bakSize}; j++ )); do
+for (( j=0; j<bakSize-5; j++ )); do
     thisOldConfig=${arrOpenclashConfigBakup[$j]}
     # https://www.google.com/search?q=bash+string+compare+n+characters&pws=0&gl=us&gws_rd=cr
     substring="${thisOldConfig:0:n}"
