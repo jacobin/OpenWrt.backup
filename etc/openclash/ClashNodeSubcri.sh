f8768c87f179a8c8b6cba260b64696322731909f
Jacobin <harly.he@gmail.com>
2026-02-12 13:50:26 +0800

    不至于遗失的历史打包tar_old_files

--- etc/openclash/ClashNodeSubcri.sh
+++ etc/openclash/ClashNodeSubcri.sh
@@ -344,6 +344,7 @@ cp -f "${DIR0}/ClashNodeSubcri.cfg" "/etc/config/openclash"
 tar_old_files "/etc/config/openclash.backup" "/etc/config/openclash.2*" 5
 tar_old_files "/etc/openclash/yamls" "/etc/openclash/*.yaml" 2
 tar_old_files "/etc/openclash/wget.log" "/etc/openclash/wget-log*" 1
+tar_old_files "/etc/openclash/config/config_yamls" "/etc/openclash/config/*.yaml" 1
 
 
 ###############################################################################
@@ -506,6 +507,10 @@ function tar_old_files() {
     targetFPathMatchingPattern=$(echo "$2" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
     nReserve=$(echo "$3" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
 
+    folder1="$(dirname "${tarFPath}")"
+    folder2="$(dirname "${targetFPathMatchingPattern}")"
+    assert_true "[[ ${folder1} == ${folder2} ]]" "\"${folder1}\" and \"${folder2}\" must have the same parent folder"
+
     # It must be ensured that "tarFPath" is not in the pattern matching of "targetFPathMatchingPattern"
 
     if [ -f "${tarFPath}.tar.gz" ]; then
@@ -517,8 +522,36 @@ function tar_old_files() {
         assert_true "[ ! -f \"${tarFPath}.tar\" ]" "Delete file \"${tarFPath}.tar\" failed."
     fi
 
-    readarray -t arrOpenclashConfigBakup < <(ls -t -r -1 ${targetFPathMatchingPattern})
+    ListFPathTemp=$(mktemp "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
+    ls -t -r -1 ${targetFPathMatchingPattern} > ${ListFPathTemp} 2>/dev/null
+    ls -t -r -1 ${targetFPathMatchingPattern}.2???????_?????? >> ${ListFPathTemp} 2>/dev/null
+    readarray -t arrOpenclashConfigBakup < <(cat "${ListFPathTemp}")
+    rm "${ListFPathTemp}" -f
     bakSize=${#arrOpenclashConfigBakup[@]}
+
+    NowDatetime="$(date +'%Y%m%d_%H%M%S')"
+    for (( j=0; j<bakSize; j++ )); do
+        fpath=${arrOpenclashConfigBakup[$j]}
+        fpathLen=${#fpath}
+
+        # bRename
+        bRename=false
+        if (( 15 < fpathLen )); then
+            last15chars=${fpath: -15}
+            if ! is_valid_datetime "${last15chars}"; then
+                bRename=true
+            fi
+        else
+            bRename=true
+        fi
+
+        if [[ "${bRename}" == "true" ]]; then
+            newFpath="${fpath}.${NowDatetime}"
+            mv -f "${fpath}" "${newFpath}" &> /dev/null
+            arrOpenclashConfigBakup[$j]="${newFpath}"
+        fi
+    done
+
     tar_command_string="tar c -v -f \"${tarFPath}.tar\""
     rm_command_string="rm -f"
     for (( j=0; j<bakSize-nReserve; j++ )); do
@@ -534,6 +567,35 @@ function tar_old_files() {
         assert_true "[[ -f \"${tarFPath}.tar.gz\" && ! -f \"${tarFPath}.tar\" ]]" "Compressed file \"${tarFPath}.tar\" failed."
     fi
 }
+
+
+###############################################################################
+######################### function: is_valid_datetime #########################
+###############################################################################
+function is_valid_datetime() {
+    # OpenWrt uses the busybox-date utility to manage time, supporting formats
+    # like YYYY-MM-DD hh:mm[:ss], [YYYY.]MM.DD-hh:mm[:ss],
+    # or [[[[[YY]YY]MM]DD]hh]mm[.ss]. The command date -s is used to manually
+    # set the system time, while ntpd -q -p is used for syncing via NTP.
+    date -D "%Y%m%d_%H%M%S" "$1" > NUL 2>&1
+    if [ $? -eq 0 ]; then
+        return 0
+    fi
+
+    return 1
+}
+# # Example Usage:
+# function Testcase() {
+#     if is_valid_datetime "$1"; then
+#         echo "'$1' is a valid date."
+#     else
+#         echo "'$1' is not a valid date."
+#     fi
+# }
+#
+# Testcase "20240228_120342"
+# Testcase "20240231_120342"
+# Testcase "20240232_120342"
 
 
 ###############################################################################
