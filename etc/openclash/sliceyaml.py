f30a1c65d9e97494f79a195d3bdb69f394579ce4
Jacobin <harly.he@gmail.com>
2026-07-06 10:56:14 +0800

    pip list

--- etc/openclash/sliceyaml.py
+++ etc/openclash/sliceyaml.py
@@ -11,6 +11,55 @@ import re
 from pathvalidate import is_valid_filename #, validate_filename
 from typing import Dict, List, Any, Optional
 
+# root@OpenWrt:~# pip list
+# Package             Version
+# ------------------- ----------
+# aiohttp             3.9.0
+# aiosignal           1.4.0
+# async-timeout       4.0.3
+# attrs               25.4.0
+# autoflake           2.3.1
+# autopep8            2.3.2
+# bcrypt              3.1.7
+# beautifulsoup4      4.14.3
+# certifi             2025.11.12
+# cffi                1.14.5
+# charset-normalizer  3.4.4
+# click               8.1.8
+# commentjson         0.9.0
+# dnspython           2.7.0
+# frozenlist          1.8.0
+# geoip2              5.1.0
+# goto-statement      1.2
+# idna                3.11
+# lark-parser         0.7.8
+# lxml                5.0.0
+# maxminddb           2.8.2
+# multidict           6.7.0
+# packaging           25.0
+# pathvalidate        3.3.1
+# pip                 25.3
+# ply                 3.11
+# portalocker         3.2.0
+# propcache           0.4.1
+# pycodestyle         2.14.0
+# pycparser           2.20
+# pyflakes            3.4.0
+# PyYAML              5.3.1
+# requests            2.32.5
+# requirements-parser 0.13.0
+# setuptools          80.9.0
+# six                 1.16.0
+# soupsieve           2.8
+# speedtest-cli       2.1.3
+# texttable           1.6.3
+# tomli               2.4.0
+# typing_extensions   4.15.0
+# tzdata              2025.3
+# urllib3             2.6.2
+# wheel               0.45.1
+# yarl                1.22.0
+
 ###############################################################################
 def load_yaml_config(file_path: str) -> Dict[str, Any]:
     try:
@@ -66,7 +115,7 @@ def main():
     proxyCount = len( all_proxies )
     if proxyCount == 0:
         return
-        
+
     arrSliceSize = []
     arrSliceSize.append( 0 )
     # range(start, stop, step) --
