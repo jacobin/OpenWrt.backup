716b0ee2ef63841987abd9f262ec1d15beae6af6
Jacobin <harly.he@gmail.com>
2026-07-18 18:56:01 +0800

    再增长一倍大小的日志

--- etc/openclash/ClashNodeSubcri.sh
+++ etc/openclash/ClashNodeSubcri.sh
@@ -95,7 +95,7 @@ for fiLe in "${existing_files[@]}"; do
 done
 
 file_hash=$(sha256sum "${DIR0}/ClashNodeSubcri.etc_config_openclash.const" 2>/dev/null | awk '{print $1}')
-if [ "$file_hash" != "4dbbca73113571ac45865e12b91b634c535c83b4f13cde958b50cd86825033ea" ]; then
+if [ "$file_hash" != "8d561d0cb757bc0258787f8e9fdf0404216a750dda69df0ee0701e32bcefd686" ]; then
     tee_echo "\tThe SHA256 of file \"${DIR0}/ClashNodeSubcri.etc_config_openclash.const\" is incorrect, please check"
     singleton_clean_up 1
 fi
